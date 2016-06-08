5e41cc23d0f8ac979e370090757e4a5296ba4939
Allen Ko <allenwko@allens-mbp.attlocal.net>
2016-06-08 15:54:34 -0700

    Python 1 exercise completed @sampathweb

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -1,19 +1,44 @@
 #The football.csv file contains the results from the English Premier League. 
-# The columns labeled ‘Goals’ and ‘Goals Allowed’ contain the total number of 
 # goals scored for and against each team in that season (so Arsenal scored 79 goals 
 # against opponents, and had 36 goals scored against them). Write a program to read the file, 
-# then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
 
 # The below skeleton is optional.  You can use it or you can write the script with an approach of your choice.
 
 
 import csv
 
-  def read_data(data):
-   # COMPLETE THIS FUNCTION
 
-  def get_min_score_difference(self, parsed_data):
-    # COMPLETE THIS FUNCTION
+def read_data(filename):
+    """
+    Takes a csvfile and returns a list of dictionaries
+    """
+    
+    data_list=[]
 
-  def get_team(self, index_value, parsed_data):
-    # COMPLETE THIS FUNCTION
+    with open(filename, 'rb') as footfile:
+        footreader = csv.DictReader(footfile, delimiter = ",")
+        for row in footreader:
+            data_list.append(row)
+    
+    return data_list
+
+def get_min_score_difference(parsed_data):
+    """
+    Takes a list of dictionaries and sorts the list by the difference in goals
+    """
+    new_list = sorted(parsed_data, key = lambda x: abs(int(x['Goals'])-int(x['Goals Allowed'])))
+    # code for checking output of sorted list
+    #for i in new_list:
+    #    print i['Team'], abs(int(i['Goals'])-int(i['Goals Allowed']))
+    return new_list
+
+def get_team(parsed_data):
+    """
+    Takes the sorted list of dictionaries and returns the team in the first element
+    """
+    return parsed_data[0]['Team']
+
+filename = 'football.csv'
+foot_data = read_data(filename)
+sorted_foot_data = get_min_score_difference(foot_data)
+print get_team(sorted_foot_data)
